fd7d0a61baab80b3dd40f3f682e812fe1684aca1
Ihor Ryabets <eronryabets@gmail.com>
2022-02-20 19:23:32 +0200

    Saving passwords in the database

--- src/main/java/com/eronryabets/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/com/eronryabets/spring/security/configuration/MySecurityConfig.java
@@ -1,5 +1,6 @@
 package com.eronryabets.spring.security.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -7,17 +8,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
-        auth.inMemoryAuthentication()
-                .withUser(userBuilder.username("eron").password("eron").roles("EMPLOYEE"))
-                .withUser(userBuilder.username("elena").password("elena").roles("HR"))
-                .withUser(userBuilder.username("ivan").password("ivan").roles("MANAGER","HR"));
-    }
+        auth.jdbcAuthentication().dataSource(dataSource);
 
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -28,3 +31,14 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
                 .and().formLogin().permitAll();
     }
 }
+
+/*
+  @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser(userBuilder.username("eron").password("eron").roles("EMPLOYEE"))
+                .withUser(userBuilder.username("elena").password("elena").roles("HR"))
+                .withUser(userBuilder.username("ivan").password("ivan").roles("MANAGER","HR"));
+    }
+ */
